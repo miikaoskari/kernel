8a585a2b31bf952fc7c55d08dff423a8f512ab05
miikaoskari <miika.karjalainen13@gmail.com>
2024-08-27 23:07:40 +0300

    add -serial pty argument

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-qemu-system-aarch64 -M raspi4b -serial stdio -kernel os.elf -s -S &
+qemu-system-aarch64 -M raspi4b -serial stdio -serial pty -kernel os.elf -s -S &
 lldb -o "target create os.elf" -o "gdb-remote 1234"
