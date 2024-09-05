6fac8895b7b741408f5c284839617f2487cd8598
miikaoskari <miika.karjalainen13@gmail.com>
2024-09-05 20:41:18 +0300

    refactor: Update kernel file name in run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-qemu-system-aarch64 -M raspi4b -serial stdio -serial pty -kernel os.elf -s -S &
-lldb -o "target create os.elf" -o "gdb-remote 1234"
+qemu-system-aarch64 -M raspi4b -serial stdio -serial pty -kernel kernel8.elf -s -S &
+lldb -o "target create kernel8.elf" -o "gdb-remote 1234"
