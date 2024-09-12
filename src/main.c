7dfd03c4783f9758a07402f1628f691c71716083
miika <miika.karjalainen13@gmail.com>
2024-09-12 19:18:25 +0200

    use printk instead of uart_puts

--- src/main.c
+++ src/main.c
@@ -2,10 +2,13 @@
 #include <stdint.h>
 
 #include "uart/uart.h"
+#include "utils/printk/printk.h"
 
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
 #endif
+
+#define RP4 4
 
 #ifdef AARCH64
 // arguments for AArch64
@@ -15,8 +18,10 @@ void kmain(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3)
 void kmain(uint32_t r0, uint32_t r1, uint32_t atags)
 #endif
 {
-  uart_init(4);
-  uart_puts("Hello, kernel World!\r\n");
+  uart_init(RP4);
+  set_putc((putc_func_t)uart_putc);
+
+  printk("Hello, world!\n");
 
   while (1) {
     uart_putc(uart_getc());
