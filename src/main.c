de998fb57bf7faf328bd113f506ba7c02454e07c
miika <miika.karjalainen13@gmail.com>
2024-09-12 19:26:42 +0200

    working exception level get function

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@ extern "C" /* Use C linkage for kernel_main. */
 
 #define RP4 4
 
+extern int get_el(void);
+
 #ifdef AARCH64
 // arguments for AArch64
 void kmain(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3)
@@ -22,6 +24,9 @@ void kmain(uint32_t r0, uint32_t r1, uint32_t atags)
   set_putc((putc_func_t)uart_putc);
 
   printk("Hello, world!\n");
+
+  int el = get_el();
+  printk("Current exception level: %d\n", el);
 
   while (1) {
     uart_putc(uart_getc());
