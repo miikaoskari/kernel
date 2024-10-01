1a6d9b1d155b1e9117fc0d9690686ede179316d0
miikaoskari <miika.karjalainen13@gmail.com>
2024-10-01 13:47:29 +0300

    add timer peripheral

---
+++ src/peripherals/timer/timer.c
@@ -0,0 +1,19 @@
+#include "peripherals/timer/timer.h"
+#include "mmio/mmio.h"
+
+const unsigned int interval = 200000;
+unsigned int current_time = 0;
+
+void timer_init(void)
+{
+    current_time = mmio_read(TIMER_CLO);
+    current_time += interval;
+    mmio_write(TIMER_C1, current_time);
+}
+
+void handle_timer_irq(void)
+{
+    current_time += interval;
+    mmio_write(TIMER_C1, current_time);
+    mmio_write(TIMER_CS, TIMER_CS_M1);
+}
