3fc03c60e9255d48748059e6aa2df1087f33ecd8
miikaoskari <miika.karjalainen13@gmail.com>
2024-10-06 18:20:40 +0300

    use enum for timer registers

--- src/peripherals/bcm2711/timer/timer.c
+++ src/peripherals/bcm2711/timer/timer.c
@@ -5,6 +5,24 @@
 const unsigned int interval = 200000;
 unsigned int current_time = 0;
 
+enum
+{
+    TIMER_BASE = 0x7E003000,
+
+    TIMER_CS = (TIMER_BASE + 0x00000000),
+    TIMER_CLO = (TIMER_BASE + 0x00000004),
+    TIMER_CHI = (TIMER_BASE + 0x00000008),
+    TIMER_C0 = (TIMER_BASE + 0x0000000C),
+    TIMER_C1 = (TIMER_BASE + 0x00000010),
+    TIMER_C2 = (TIMER_BASE + 0x00000014),
+    TIMER_C3 = (TIMER_BASE + 0x00000018),
+
+    TIMER_CS_M0 = (1 << 0),
+    TIMER_CS_M1 = (1 << 1),
+    TIMER_CS_M2 = (1 << 2),
+    TIMER_CS_M3 = (1 << 3),
+};
+
 void timer_init(void)
 {
     current_time = mmio_read(TIMER_CLO);
