b75a36581489debc23c81e71fb80343937f3ec0b
miikaoskari <miika.karjalainen13@gmail.com>
2024-12-13 00:02:06 +0200

    implement GIC interrupt handling for raspi4b

--- src/irq/irq.c
+++ src/irq/irq.c
@@ -1,7 +1,14 @@
 #include "mmio/mmio.h"
 #include "utils/printk/printk.h"
-#include "peripherals/bcm2711/irq.h"
 #include "peripherals/bcm2711/timer/timer.h"
+#include "peripherals/bcm2711/irq.h"
+
+#define CPU_INTERFACE_0 (0)
+#define CPU_INTERFACE_1 (1)
+#define CPU_INTERFACE_2 (2)
+#define CPU_INTERFACE_3 (3)
+
+#define MAX_INTERFACE_COUNT (4)
 
 const char *entry_error_messages[] = {
     "SYNC_INVALID_EL1t",
@@ -25,10 +32,39 @@ const char *entry_error_messages[] = {
     "ERROR_INVALID_EL0_32",
 };
 
+void enable_irq(unsigned int interrupt_id)
+{
+    /* The corresponding GICD_ISENABLER<n> number, n, is given by n = m DIV 32. */
+    unsigned int n = interrupt_id / 32;
+    /* The offset of the required GICD_ISENABLER is (0x100 + (4*n)). */
+    unsigned int register_offset = GICD_ISENABLER_BASE + (4 * n);
+    /* The bit number of the required interrupt is m MOD 32. */
+    unsigned int bit_number = interrupt_id % 32;
+    /* Set the corresponding bit in the GICD_ISENABLER<n> register. */
+    mmio_write(register_offset, 1 << bit_number);
+}
+
+
+/* For interrupt ID m, when DIV and MOD are the integer division and modulo operations. */
+void assign_target_cpu(unsigned int interrupt_id, unsigned int cpu_id)
+{
+    /* The corresponding GICD_ITARGETSR<n> number, n, is given by n = m DIV 4. */
+    unsigned int n = interrupt_id / 4;
+    /* The offset of the required GICD_ITARGETSR<n> register is (0x800 + (4*n)) */
+    unsigned int register_offset = GICD_ISENABLER_BASE + (4 * n);
+    /* Get current value of the register */
+    unsigned int current_register_value = mmio_read(register_offset);
+    /* Set the correct CPU target */
+    mmio_write(register_offset, current_register_value | 1 << cpu_id);
+
+}
+
+
 void enable_interrupt_controller()
 {
     /* enable system timer irq */
-    mmio_write(ENABLE_IRQS_1, SYSTEM_TIMER_IRQ_1);
+    assign_target_cpu(SYSTEM_TIMER_IRQ_1, CPU_INTERFACE_0);
+    enable_irq(SYSTEM_TIMER_IRQ_1);
 }
 
 void show_invalid_entry_message(int type, unsigned long esr, unsigned long address)
@@ -38,12 +74,18 @@ void show_invalid_entry_message(int type, unsigned long esr, unsigned long addre
 
 void handle_irq()
 {
-    unsigned int irq = mmio_read(IRQ_PENDING_1);
+    /* Get the interrupt acknowledge register */
+    unsigned int irq_ack = mmio_read(GICC_IAR);
+    /* Get the interrupt ID
+     * INTIDs 1020-1023 are reserved and convey additional information
+     * such as spurious interrupts. */
+    unsigned int irq = irq_ack & 0x3ff;
     printk("irq pending: %x\r\n", irq);
     switch (irq)
     {
     case (SYSTEM_TIMER_IRQ_1):
         handle_timer_irq();
+        mmio_write(GICC_EOIR, irq_ack);
         break;
     default:
         printk("Unknown pending irq: %x\r\n", irq);
