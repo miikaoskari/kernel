375ef6ef2748f61b551dd73aa97c146564ca180f
miika <miika.karjalainen13@gmail.com>
2024-12-21 10:18:39 +0200

    add complete memory reads and strict align defines

---
+++ src/peripherals/bcm2711/cpu.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/* To ensure we loaded the correct data, we must have the CPU wait for pending
+ * reads before moving onto reads from other peripherals. This is done with a
+ * memory barrier instruction. */
+#define COMPLETE_MEMORY_READS __asm__ volatile ("dsb sy")
+#define STRICT_ALIGN __attribute__((target("strict-align")))
+
