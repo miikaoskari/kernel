0603cef4417cd6f755dd9a891e0215ee7fe01826
miika <miika.karjalainen13@gmail.com>
2025-01-31 18:26:33 +0200

    add some documentation/comments

--- src/mmio/mmio.c
+++ src/mmio/mmio.c
@@ -1,8 +1,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/**
+ * @brief Base address for MMIO
+ */
 static uint32_t MMIO_BASE;
 
+/**
+ * @brief Initialize the MMIO base address based on the Raspberry Pi model.
+ *
+ * @param type The type of Raspberry Pi:
+ * - 2 or 3: Sets MMIO_BASE to 0x3F000000
+ * - 4: Sets MMIO_BASE to 0xFE000000
+ * - Default: Sets MMIO_BASE to 0x20000000 (for Raspberry Pi 1, Raspberry Pi Zero, etc.)
+ */
 void mmio_init(int type) {
   switch (type) {
   case 2:
@@ -18,10 +29,22 @@ void mmio_init(int type) {
   }
 }
 
+/**
+ * @brief Write a 32-bit value to a MMIO register.
+ *
+ * @param reg The register offset from the MMIO base address.
+ * @param data The 32-bit data to write to the register.
+ */
 void mmio_write(uint32_t reg, uint32_t data) {
   *(volatile uint32_t *)(MMIO_BASE + reg) = data;
 }
 
+/**
+ * @brief Read a 32-bit value from a MMIO register.
+ *
+ * @param reg The register offset from the MMIO base address.
+ * @return The 32-bit value read from the register.
+ */
 uint32_t mmio_read(uint32_t reg) {
   return *(volatile uint32_t *)(MMIO_BASE + reg);
 }
