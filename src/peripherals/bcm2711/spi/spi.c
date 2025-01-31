bd1e5d575c83acd7d411fe00edfb6d5dcfe06db9
miika <miika.karjalainen13@gmail.com>
2025-01-31 18:25:26 +0200

    add spi driver implementation (untested)

--- src/peripherals/bcm2711/spi/spi.c
+++ src/peripherals/bcm2711/spi/spi.c
@@ -14,22 +14,57 @@
  * - If RXR is set read 48 bytes data from SPI_FIFO and if more data to write, write up to 48 bytes to SPI_FIFO.
  */
 
+static SPI0_Type spi0;
+
 /*
  * Initialize SPI0 peripheral as interrupt driven
  */
 void spi_init(void)
 {
-    SPI0_Type spi0;
-
-    /* Set INTR and INTD */
-    spi0.CS_b.INTR = true;
-    spi0.CS_b.INTD = true;
-
     /* Set CS, CPOL, CPHA as required */
     spi0.CS_b.CS = 0;
     spi0.CS_b.CPOL = 0;
     spi0.CS_b.CPHA = 0;
+}
 
-    /* Set TA */
+void spi_enable_interrupts(void) {
+    spi0.CS_b.INTR = true;
+    spi0.CS_b.INTD = true;
+}
+
+void spi_write(uint8_t *data, uint16_t len) {
+    for (int i = 0; i < len; i++) {
+        /* wait until tx fifo has space */
+        while (!spi0.CS_b.TXD);
+
+        /* write data to FIFO */
+        spi0.FIFO = data[i];
+    }
+    /* set transfer active */
     spi0.CS_b.TA = true;
+
+    /* wait for transfer to complete */
+    while (!spi0.CS_b.DONE);
+
+    /* clear status */
+    spi0.CS_b.TA = false;
+}
+
+void spi_read(uint8_t *data, uint16_t len) {
+    for (int i = 0; i < len; i++) {
+        /* set transfer active */
+        spi0.CS_b.TA = true;
+
+        /* wait until rx fifo has data */
+        while (!spi0.CS_b.RXD);
+
+        /* read data from FIFO */
+        data[i] = spi0.FIFO;
+    }
+
+    /* wait for transfer to complete */
+    while (!spi0.CS_b.DONE);
+
+    /* clear status */
+    spi0.CS_b.TA = false;
 }
---
+++ src/peripherals/bcm2711/spi/spi.h
@@ -0,0 +1,11 @@
+#ifndef P_SPI_H
+#define P_SPI_H
+
+#include <stdint.h>
+
+void spi_init(void);
+void spi_enable_interrupts(void);
+void spi_write(uint8_t *data, uint16_t len);
+void spi_read(uint8_t *data, uint16_t len);
+
+#endif
