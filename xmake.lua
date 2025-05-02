a3dc80a7f9f286596ac63609ece642ccd8c83a37
miikaoskari <miika.karjalainen13@gmail.com>
2025-05-02 23:01:26 +0300

    Implement xmake build configuration

---
+++ xmake.lua
@@ -0,0 +1,32 @@
+add_rules("mode.debug", "mode.release")
+
+toolchain("aarch64-none-elf")
+    set_toolset("cc", "aarch64-none-elf-gcc")
+    set_toolset("as", "aarch64-none-elf-gcc")
+    set_toolset("ld", "aarch64-none-elf-ld")
+toolchain_end()
+
+-- set the toolchain and language
+set_toolchains("aarch64-none-elf")
+set_arch("aarch64")
+set_languages("gnu17")
+
+-- define the kernel target
+target("kernel8.elf")
+    set_kind("binary")
+
+    add_files("src/**/*.c",
+    "src/**/*.S",
+    "arch/aarch64/**/*.c",
+    "arch/aarch64/**/*.S",
+    "arch/aarch64/*.S",
+    "arch/aarch64/*.c")
+    add_files("external/printk/*.c")
+    add_files("linker8.ld")
+
+    add_includedirs("src",
+    "arch/aarch64",
+    "external/printk")
+
+    add_cflags("-ffreestanding", {force = true})
+    add_cflags("-Wall", "-Wextra")
